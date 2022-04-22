7ac580b3cdd8c0d3db38c88265b80baa9f9e8d71
Nashira Deer <velocicraft2088@gmail.com>
2022-04-22 19:50:53 +0000

    Configure debugger and app initializer

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,57 @@
+console.log("[APP]: Initializing Hellion Warden...");
 import * as winston from 'winston';
-import * as Discord from 'discord.js';
-import * as argparse from 'argparse';
+import { ArgumentParser } from 'argparse';
+import { writeFileSync, mkdirSync, existsSync } from 'fs';
+import { resolve as pathResolve } from 'path';
 
-const argparser = new argparse.ArgumentParser({
+interface HellionWardenArgs {
+   datadir: string
+}
+
+interface HellionWardenConfig {
+   prefix: string,
+   token: string,
+   console: boolean,
+   logfile: string
+}
+
+console.log("[APP]: Parsing command line...");
+const argparser = new ArgumentParser({
    description: 'Hellion Warden is a Discord Music Bot developed by Nashira Deer'
 });
 
-argparser.add_argument('-p', '--prefix', {
-   metavar: 'PREFIX',
+argparser.add_argument('datadir', {
    type: 'str',
-   help: 'Override the default bot prefix',
-   default: 'h!'
+   help: 'Set a directory to be used to storage and load the configuration file.'
 });
 
-argparser.add_argument('--logfile', {
-   metavar: 'FILE',
-   type: 'str',
-   help: 'Create and add a file to the logger'
-});
+let args: HellionWardenArgs = argparser.parse_args();
+let datapath = pathResolve(args.datadir);
 
-argparser.add_argument('--noconsole', {
-   help: 'Create and add a file to the logger'
-});
+console.log("[APP]: Loading...");
+if (!existsSync(datapath))
+{
+   mkdirSync(datapath);
+   console.warn("[APP]: Creating data directory...");
+}
+
+let configpath = pathResolve(datapath, "config.json");
+if (!existsSync(configpath))
+{
+   console.error("[APP]: Configuration file don't exists, creating one...");
+
+   writeFileSync(configpath, JSON.stringify({
+      prefix: 'h!',
+      token: '',
+      console: true,
+      file: null
+   }, null, 4));
+
+   console.log(`[APP]: Edit '${configpath}' before running Hellion Warden again.`);
+   process.exit(1);
+}
+else
+{
+   const CONFIG: HellionWardenConfig = require(configpath);
+
+}
