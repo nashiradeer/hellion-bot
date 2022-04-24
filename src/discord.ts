67beaa4f33923e3f86d6504264ae068e377c4b80
Nashira Deer <velocicraft2088@gmail.com>
2022-04-24 02:47:13 -0300

    Better formatting in HellionWarden

--- src/discord.ts
+++ src/discord.ts
@@ -1,4 +1,3 @@
-import { commandHandler } from '.';
 import { Client, ClientOptions, CommandInteraction, Interaction, Message } from 'discord.js';
 import { EventEmitter } from 'events';
 import { resolve } from 'path';
@@ -38,38 +37,26 @@ export class HellionWarden extends EventEmitter
 
         // Initialize
         this.emit('debug', 'info', "Initializing...");
-
         this._token = token;
         this.prefix = prefix;
-
         this._data = {
             musicdata: new Map<string, any>()
         };
 
         // Initialize Discord Client
         this.emit('debug', 'info', "Initializing Discord Client...");
-
         this._client = new Client(options || { intents: [ 'GUILDS', 'GUILD_MESSAGES', 'GUILD_VOICE_STATES' ]});
 
         // Register Discord Client events
         this.emit('debug', 'debug', "Registering Discord Client events...");
-
         this._client.on('messageCreate', (message) => this.message(message));
-
-        this._client.on('interactionCreate', this.interaction);
-
+        this._client.on('interactionCreate', (interaction) => this.interaction(interaction));
         this._client.once('ready', () => {
-            this.commandHandler.registerSlash();
             this.emit('logged');
         });
 
         // Initialize Command Handler
         this.emit('debug', 'info', "Initializing Command Handler...");
-        this.commandHandler = new commandHandler.HellionCommandHandler(this._client, resolve(__dirname, 'commands'));
-
-        this.commandHandler.once('ready', () => {
-            this.emit('ready');
-        });
     }
 
     private async message(message: Message): Promise<void>
@@ -82,12 +69,13 @@ export class HellionWarden extends EventEmitter
         this.commandHandler.run(message, this.prefix, this._data);
     }
 
-    private async interaction(interact: Interaction)
+
+    private async interaction(interaction: Interaction)
     {
-        if (interact.isCommand())
+        if (interaction.isCommand())
         {
             this.emit('debug', 'debug', 'Running command from a interaction.');
-            this.commandHandler.runInteraction(interact as CommandInteraction, this._data);
+            this.commandHandler.runInteraction(interaction as CommandInteraction, this._data);
         }
     }
 
