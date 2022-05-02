ac891e95122ee479248e4b4978d330a0f0a17f9e
Nashira Deer <velocicraft2088@gmail.com>
2022-05-02 02:00:13 -0300

    Bump version

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,10 @@ export * as logger from './modules/logger';
 export * as commandHandler from './modules/command';
 export * as player from './modules/player';
 
-export declare interface HellionWardenInformation
-{
+export declare interface HellionWardenInformation {
     VERSION: string;
 }
 
 export const HellionWardenInformation = {
-    VERSION: "v1.0.0"
+    VERSION: "v1.0.1"
 };
