2b74131b5dc2b02ae5d2d3fd85ada2430f7df6fd
Nashira Deer <velocicraft2088@gmail.com>
2022-05-29 12:56:00 -0300

    Better time processing in about command

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -22,17 +22,37 @@ export class HellionCommand extends commandHandler.HellionCommandListener {
                         `Version: ${HellionWardenInformation.VERSION}`)
                     .addField("NodeJS Information",
                         `Version: ${process.version}\n` +
-                        `Uptime: ${Math.floor(process.uptime() / 60)} mins\n` +
+                        `Uptime: ${this.getTime(process.uptime())}\n` +
                         `Operation System: ${process.platform}\n` +
                         `Architecture: ${process.arch}`
                         , true)
                     .addField("Discord.JS Information",
                         `Version: ${version}\n` +
-                        `Uptime: ${Math.floor((event.client.uptime || 0) / 1000 / 60)} mins\n` +
+                        `Uptime: ${this.getTime((event.client.uptime || 0) / 1000)} mins\n` +
                         `API Latency: ${event.client.ws.ping}ms\n` +
                         `Latency: ${Date.now() - event.createdTimestamp}ms`
                         , true)
             ]
         });
+    }
+
+    private getTime(secs: number): string {
+        if (secs >= 86400) {
+            let t = Math.floor(secs / 60 / 60 / 24);
+            if (t > 1) return `${Math.floor(t)} days`;
+            else return `${Math.floor(t)} day`;
+        }
+        if (secs >= 3600) {
+            let t = Math.floor(secs / 60 / 60);
+            if (t > 1) return `${Math.floor(t)} hours`;
+            else return `${Math.floor(t)} hour`;
+        }
+        if (secs >= 60) {
+            let t = Math.floor(secs / 60);
+            if (t > 1) return `${Math.floor(t)} mins`;
+            else return `${Math.floor(t)} min`;
+        }
+        if (secs > 1) return `${Math.floor(secs)} secs`;
+        else return `${Math.floor(secs)} sec`;
     }
 }
