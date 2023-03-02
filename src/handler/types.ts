2d3e9c185711ee73afcff656f39c0dc2d4a58375
NashiraDeer <nashiradeer@deersoftware.dev>
2023-03-02 15:01:51 +0000

    Use Promise<void> instead of PromiseLike<void>

--- src/handler/types.ts
+++ src/handler/types.ts
@@ -4,27 +4,27 @@ import { HellionHandler } from "./handler";
 export interface HellionCommand {
     name: string;
     data: () => SlashCommandBuilder;
-    execute: (interaction: ChatInputCommandInteraction, handler: HellionHandler) => void | PromiseLike<void>;
+    execute: (interaction: ChatInputCommandInteraction, handler: HellionHandler) => void | Promise<void>;
 }
 
 export interface HellionAutocomplete {
-    autocomplete: (interaction: AutocompleteInteraction, handler: HellionHandler) => void | PromiseLike<void>;
+    autocomplete: (interaction: AutocompleteInteraction, handler: HellionHandler) => void | Promise<void>;
 }
 
 export interface HellionContextMenu {
     contextMenuName: string;
     data: () => ContextMenuCommandBuilder;
-    execute: (interaction: ContextMenuCommandInteraction, handler: HellionHandler) => void | PromiseLike<void>;
+    execute: (interaction: ContextMenuCommandInteraction, handler: HellionHandler) => void | Promise<void>;
 }
 
 export interface HellionComponent {
     componentId: string;
-    execute: (interaction: MessageComponentInteraction, handler: HellionHandler) => void | PromiseLike<void>;
+    execute: (interaction: MessageComponentInteraction, handler: HellionHandler) => void | Promise<void>;
 }
 
 export interface HellionModal {
     modalId: string;
-    execute: (interaction: ModalSubmitInteraction, handler: HellionHandler) => void | PromiseLike<void>;
+    execute: (interaction: ModalSubmitInteraction, handler: HellionHandler) => void | Promise<void>;
 }
 
 export type HellionListener = HellionCommand | HellionCommand & HellionAutocomplete | HellionComponent | HellionModal | HellionContextMenu;
